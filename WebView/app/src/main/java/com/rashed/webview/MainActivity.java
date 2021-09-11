2ae5533e9de1a6330f6a610cfbcd87f49ceb9596
Hasan Rashed <hasanrashed537@gmail.com>
2021-09-11 15:34:07 +0600

    WebView Added

---
+++ WebView/app/src/main/java/com/rashed/webview/MainActivity.java
@@ -0,0 +1,28 @@
+package com.rashed.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity
+{
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://britannia.edu.bd/");
+    }
+}
