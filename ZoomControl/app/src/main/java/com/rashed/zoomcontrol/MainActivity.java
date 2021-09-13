eca653eb84fb04a804faafcde0303564ef499bb6
Hasan Rashed <hasanrashed537@gmail.com>
2021-09-13 06:48:21 +0600

    ZoomControl Added

---
+++ ZoomControl/app/src/main/java/com/rashed/zoomcontrol/MainActivity.java
@@ -0,0 +1,61 @@
+    package com.rashed.zoomcontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.ZoomControls;
+
+    public class MainActivity extends AppCompatActivity
+{
+    private ImageView imageView;
+    private ZoomControls zoomControls;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        zoomControls = findViewById(R.id.zoomControl);
+
+        zoomControls.setOnZoomInClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                float x = imageView.getScaleX();
+                float y = imageView.getScaleY();
+
+                if (x < 5 && y < 5)
+                {
+                    imageView.setScaleX((float)x + 1);
+                    imageView.setScaleY((float)y + 1);
+                }
+
+                Toast.makeText(MainActivity.this, "Zoom in", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        zoomControls.setOnZoomOutClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                float x = imageView.getScaleX();
+                float y = imageView.getScaleY();
+
+                if (x > 1 && y > 1)
+                {
+                    imageView.setScaleX((float)x - 1);
+                    imageView.setScaleY((float)y - 1);
+                }
+
+                Toast.makeText(MainActivity.this, "Zoom out", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
