992bef06bbe7eddb73d59d917ebf11641d53e8f8
Hasan Rashed <hasanrashed537@gmail.com>
2021-09-24 09:59:40 +0600

    HTML Added

---
+++ HTML/app/src/main/java/com/rashed/html/MainActivity.java
@@ -0,0 +1,37 @@
+package com.rashed.html;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity
+{
+    private WebView webView;
+    String string = "<h1> This is heading 1</h1>\n" +
+                    "<h2> This is heading 2</h2>\n" +
+                    "<h3> This is heading 3</h3>\n" +
+                    "<p> This is a paragraph</p>\n" +
+                    "<p> <u> This is an underline text </u> </p>\n" +
+                    "<p> <b> This is an bold text </b> </p>\n" +
+                    "<p> <i> This is an italic text </i> </p>\n" +
+                    "Programming language ordered list \n" +
+                    "<ol>\n" +
+                    "<li> C </li>\n" +
+                    "<li> C++ </li>\n" +
+                    "<li> Java </li>\n" +
+                    "<ol>\n" +
+                    "</ol>\n\n" +
+                    "(a + b) <sup> 2 </sup> = (a) <sup> 2 </sup> + 2ab + (b) <sup> 2 </sup>";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+        webView.loadDataWithBaseURL(null, string, "text/html", "utf-8", null);
+    }
+}
